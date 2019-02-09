f33a53a68ab5c9cd359de073af91394024d4200a
Bruno Rijsman <brunorijsman@gmail.com>
2019-02-09 15:15:19 -0600

    Only 19 bytes

--- bgp_short_message.py
+++ bgp_short_message.py
@@ -7,8 +7,7 @@ BGP_IP = '127.0.0.1'
 SHORT_MSG = (b'\xff\xff\xff\xff\xff\xff\xff\xff'     # First 8 bytes of marker
              b'\xff\xff\xff\xff\xff\xff\xff\xff'     # Last 8 bytes of marker
              b'\x00\x10'                             # Length 16
-             b'\x01'                                 # Open
-             b'\x04')                                # Version 4
+             b'\x01')                                # Open
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
